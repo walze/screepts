8fcb9f36513ef88a836e54a26bfd568a08559af4
Wallace Fares <catufuzgu@gmail.com>
2021-06-26 16:49:40 -0300

    added grunt to publish

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,15 @@ console.log(process.env.TOKEN);
 
 module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-screeps');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.initConfig({
+		watch: {
+			js: {
+				files: './main.js',
+				tasks: ['screeps'],
+			},
+		},
 		screeps: {
 			options: {
 				email: process.env.EMAIL,
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,25 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  target: 'node',
-  entry: {
-    main: path.resolve(__dirname, 'src/main.ts')
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  output: {
-    path: path.resolve(__dirname),
-    filename: 'main.js',
-    libraryTarget: 'commonjs',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        // exclude: /node_modules/
-      }
-    ]
-  },
-  // optimization: {
-    // runtimeChunk: true,
-  // },
-  plugins: [
-  ]
+	target: 'node',
+	entry: {
+		main: path.resolve(__dirname, 'src/main.ts'),
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	output: {
+		path: path.resolve(__dirname),
+		filename: 'main.js',
+		libraryTarget: 'commonjs',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				use: 'ts-loader',
+				// Exclude: /node_modules/
+			},
+		],
+	},
 };
