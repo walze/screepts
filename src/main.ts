a2d67bbb8989377d6c51b9628274ee98b21dc689
Wallace Fares <catufuzgu@gmail.com>
2021-07-29 15:25:14 -0300

    adding source counter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,37 @@
-import { keys } from 'ramda';
+import { filter, flatten, keys, length, map, pipe, tap, values } from 'ramda';
 import { makeCreep, runCreep } from './functions/creep';
+import { iff } from './helpers';
 import { ROLES } from './types';
 
 const amountCreeps = {
   [ROLES.HAVESTER]: 2,
-  [ROLES.BUILDER]: 0,
+  [ROLES.BUILDER]: 1,
   [ROLES.UPGRADER]: 0,
 };
+
+const findSpawn = (room: Room) => room.find(FIND_MY_SPAWNS, { filter: { spawning: false } })?.[0];
+
+const roomRunner = (cs: Creep[]) => (room: Room) => {
+  pipe(
+    () => keys(ROLES),
+    tap(map(role => { room.memory[role] = cs.filter(c => c.memory.role === role).length; })),
+    tap(map(iff(
+      role => room.memory[role] < amountCreeps[role],
+      makeCreep(findSpawn(room)!),
+    ))),
+  )();
+
+  return room;
+};
+
+const countHarvestable = pipe(
+  ({ pos: { x, y }, room }: Source) => room.lookAtArea(y - 1, x - 1, y + 1, x + 1),
+  o => Object.values(o),
+  map(Object.values),
+  flatten,
+  filter<LookAtResult>(({ terrain }) => terrain === 'plain' || terrain === 'swamp'),
+  length,
+);
 
 export const loop = () => {
   const { creeps: creepsObj, rooms: roomsObj } = Game;
@@ -15,20 +40,15 @@ export const loop = () => {
 
   creeps.map(runCreep);
 
-  rooms.map(room => {
-    keys(ROLES).map(role => {
-      room.memory[role] = creeps.filter(c => c.memory.role === role).length;
+  // Rooms.map(roomRunner(creeps));
 
-      if (room.memory[role] < amountCreeps[role])
-        makeCreep(room.find(FIND_MY_SPAWNS)[0]!)(role);
+  const room = rooms[0]!;
+  const source = room.find(FIND_SOURCES)[0]!;
 
-      return 0;
-    });
-
-    return 0;
-  });
-
-  console.log('------------------------');
+  console.log(
+    '------------------------',
+    countHarvestable(source),
+  );
 };
 
 module.exports = { loop };
