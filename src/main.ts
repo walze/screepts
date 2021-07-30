fcffa2e5ba791f5bae44c12202c11e241f89ee15
Wallace Fares <catufuzgu@gmail.com>
2021-07-30 08:08:58 -0300

    fixed repeting map with applicative

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { keys, map, pair, pipe, tap } from 'ramda';
+import { ap, keys, map, pipe, tap } from 'ramda';
 import { getCreeps, makeCreep, runCreep } from './functions/creep';
 import { assertThrow, iff } from './helpers';
 import { ROLE, ROLES } from './types';
@@ -9,18 +9,21 @@ const amountCreeps = {
   [ROLES.UPGRADER]: 1,
 };
 
-const findSpawn = (room: Room) => room.find(FIND_MY_SPAWNS, { filter: { spawning: null } })[0];
+const findSpawn = (room: Room) =>
+  room.find(FIND_MY_SPAWNS, { filter: { spawning: null } })[0];
 
 const setRoomCreeps
 = (cs: Creep[]) =>
   (room: Room) => pipe(
     getCreeps,
     Object.entries,
-    map(([s, css]) => pair(s as ROLE, css.length)),
-    map(([r, l]) => { room.memory[r] = l; }),
+    map(([s, css]) => {
+      room.memory[s as ROLE] = css.length;
+    }),
+    () => {},
   )(cs);
 
-const roomSpawner
+const roomCreepSpawner
   = (room: Room) =>
     pipe(
       () => keys(ROLES),
@@ -37,12 +40,14 @@ export const loop = () => {
 
   creeps.map(runCreep);
 
-  rooms.map(roomSpawner);
-  rooms.map(setRoomCreeps(creeps));
+  ap([
+    roomCreepSpawner,
+    setRoomCreeps(creeps),
+  ], rooms);
 
   console.log(
     '------------------------',
-    rooms[0]!.find(FIND_MY_SPAWNS, { filter: { spawning: null } }),
+
   );
 };
 
